d14dda1b5b575e90f02e73a0f6248a4195778665
DaneIrvine <92069721+DaneIrvine@users.noreply.github.com>
2025-06-04 08:59:16 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,324 @@
+//CONSTANTS,#10
+//#1
+const levels = [tilemap`level0`, tilemap`level4`, tilemap`level5`]
+//#9
+const dash_length = 30
+const fly_projectile_speed = 50
+const fly_projectile_turnrate = 20
+const camera_shake_amount = 3
+const hero_velocity = 100
+//GLOBALS
+let levelNumber = 0
+let enemiesleft: Sprite[] = []
+let enemiesright: Sprite[] = []
+let snail = sprites.create(assets.image`Helpful snail`, 0)
+info.setLife(3)
+//CLASSES,#11,#12,#13
+
+namespace SpriteKind {
+    export const Obstacle = SpriteKind.create()
+    export const MrsEgan = SpriteKind.create()
+}
+
+class Hero extends sprites.ExtendableSprite {
+
+    
+    herohitPoints: number = 3
+    hit(points: number): void {
+        
+        timer.throttle("action", 1000, function() {
+           this.herohitPoints -= points  
+           info.setLife(this.herohitPoints)
+        })
+       
+
+        if (this.herohitPoints <= 0) {
+            this.destroy(effects.confetti, 250)
+            game.gameOver(false)
+        }
+}
+    constructor(image: Image, kind: number) {
+        super(image, kind)
+        this.herohitPoints = 3
+    }
+
+}
+class EnemySprite extends sprites.ExtendableSprite {
+
+    hitPoints: number
+    skin: number
+    
+    hit(points: number): void {
+        this.hitPoints -= points
+
+        if (this.hitPoints <= 0) {
+            this.destroy(effects.confetti, 250)
+        }
+    }
+
+    constructor(image: Image, kind: number) {
+        super(image, kind)
+        this.hitPoints = 2
+    }
+
+
+}
+class MrsEgan extends sprites.ExtendableSprite {
+
+    hitPoints: number
+
+    hit(points: number): void {
+        this.hitPoints -= points
+
+        if (this.hitPoints <= 0) {
+            this.destroy(effects.confetti, 250)
+        }
+    }
+
+    constructor(image: Image, kind: number) {
+        super(image, kind)
+        this.hitPoints = 5
+    }
+
+
+}
+
+//FUNCTIONS
+//#4,#7
+function dash() {
+    if (mySprite.vx < 0 || mySprite.vx > 0) {
+        if (mySprite.vx < 0) {
+            animation.runImageAnimation(mySprite, assets.animation`Dane walk dash`, 60, false)
+            mySprite.setPosition(mySprite.x -= dash_length, mySprite.y)
+            
+        }
+        if (mySprite.vx > 0) {
+            mySprite.setPosition(mySprite.x += dash_length, mySprite.y)
+            animation.runImageAnimation(mySprite, assets.animation`Dane walk dash right`, 60, false)
+        }
+        
+    }
+}
+//#3,#14
+function newCowboyLeft(x: number, y:number) {
+    let enemySprite: EnemySprite =
+        new EnemySprite(assets.image`Cowboy lizard`, SpriteKind.Enemy)
+    enemySprite.hitPoints = 3
+    tiles.placeOnTile(enemySprite, tiles.getTileLocation(x, y))
+    enemiesleft.push(enemySprite)
+}
+
+function newCowboyRight(x: number, y: number) {
+    let enemySprite: EnemySprite =
+        new EnemySprite(assets.image`Cowboy lizard facing right`, SpriteKind.Enemy)
+    enemySprite.hitPoints = 3
+    tiles.placeOnTile(enemySprite, tiles.getTileLocation(x, y))
+    enemiesright.push(enemySprite)
+}
+
+function newMrseagentrueform() {
+    let bigbad: MrsEgan =
+        new MrsEgan(assets.image`Mrs Egan True Form`, SpriteKind.Enemy)
+        bigbad.hitPoints = 5
+        bigbad.follow(mySprite, 50, 50)
+        tiles.placeOnTile(bigbad, tiles.getTileLocation(11, 4))
+        animation.runImageAnimation(bigbad, assets.animation`Mrs Egan Running Down`, 120, true)
+        game.onUpdate(function() {
+           //if (bigbad.vy > 0 && bigbad.vy > bigbad.vx) {
+            
+        //}  
+        })
+       
+}
+
+
+
+
+//LEVEL CHANGE FUNCTION
+//#5
+function levelchange(levelnumber: number) {
+//LEVEL 1
+    if (levelnumber == 1) {
+        mySprite.setPosition(80, 220)
+        tiles.placeOnTile(snail, tiles.getTileLocation(3, 14))
+        snail.sayText("Since when were the cowboys lizards??!!")
+
+        let evilfly= sprites.create(assets.image`Evil Fly`, SpriteKind.Enemy)
+        tiles.placeOnTile(evilfly, tiles.getTileLocation(8, 1))
+
+
+        game.onUpdateInterval(500, function() {
+
+            if (mySprite.tilemapLocation().x >= 110) {
+                animation.runImageAnimation(evilfly, assets.animation`Evil fly flying right`, 150, false)
+            } else {
+                animation.runImageAnimation(evilfly, assets.animation`Evil fly flying left`, 150, false)
+            }
+            })
+
+    
+        let y = 4
+        for (let i = 0; i < 3; i++) {
+                newCowboyRight(1, y)
+                y += 3
+        }
+        y = 4
+        for (let i = 0; i < 3; i++) {
+                newCowboyLeft(14, y)
+                y += 3
+        }
+        
+        game.onUpdateInterval(1000, function() {
+            if (levelNumber == 1) {
+            let random = randint(0, 2)
+
+            let projectileleft= sprites.createProjectileFromSprite(assets.image`Lizard Bullet`, enemiesleft[random], -50, 0)
+            animation.runImageAnimation(enemiesleft[random], assets.animation`Cowboy lizard shooting`, 100, false)
+
+            let projectileright = sprites.createProjectileFromSprite(assets.image`Lizard Bullet`, enemiesright[random], 50, 0)
+            animation.runImageAnimation(enemiesright[random], assets.animation`Cowboy lizard shooting right`, 100, false)
+
+            console.log(levelnumber)
+
+            let projectilefly = sprites.createProjectileFromSprite(assets.image`Evil Fly Projectile`, evilfly, 0, 50)
+            projectilefly.follow(mySprite, fly_projectile_speed, fly_projectile_turnrate)
+            }
+        })
+        
+    
+    
+    }
+
+
+//LEVEL 2
+    //#6
+    if (levelnumber == 2) {
+        
+        sprites.destroy(snail)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 14))
+        sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+        
+        let enemy_smol = sprites.create(assets.image`Mrs Egan Pure Evil Final Monster`, SpriteKind.Enemy)
+        tiles.placeOnTile(enemy_smol, tiles.getTileLocation(11, 4))
+
+        scene.setBackgroundColor(15)
+
+
+        game.onUpdate(function() {
+            
+        if (mySprite.tileKindAt(TileDirection.Top, sprites.dungeon.darkGroundCenter) || mySprite.tileKindAt(TileDirection.Right, sprites.dungeon.darkGroundCenter) || mySprite.tileKindAt(TileDirection.Left, sprites.dungeon.darkGroundCenter) || mySprite.tileKindAt(TileDirection.Bottom, sprites.dungeon.darkGroundCenter))
+        {
+          
+            timer.after(500, function() {
+                
+            
+            timer.throttle("spawn1", 99999999, function() {
+            newMrseagentrueform()
+            sprites.destroy(enemy_smol)
+          })  
+        })
+        }
+        })
+
+
+    }
+}
+
+
+
+
+
+
+//EVENT HANDLERS
+
+//LEVEL CHANGE TRIGGER
+//#2
+scene.onOverlapTile(SpriteKind.Player, sprites.swamp.swampTile9, function(sprite: Sprite, location: tiles.Location) {
+    levelNumber += 1
+    levelchange(levelNumber)
+    
+    tiles.setCurrentTilemap(levels[levelNumber])
+
+   
+    if (levelNumber == 3) {
+        game.gameOver(true)
+    }
+})
+//WALL SHAKE
+scene.onHitWall(SpriteKind.Player, function(sprite: Sprite, location: tiles.Location) {
+    
+    tiles.setTileAt(location, assets.tile`Broken wall horizontal`)
+    scene.cameraShake(camera_shake_amount, 250)
+})
+//HERO DAMADGED
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function(sprite: Sprite, otherSprite: Sprite) {
+    mySprite.hit(1)
+    levelNumber == 2
+})
+//HERO DAMADGED PROJECTILE
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function(sprite: Sprite, otherSprite: Sprite) {
+    sprites.destroy(sprite)
+    mySprite.hit(1)
+})
+//HERO HITS TRAP
+scene.onOverlapTile(SpriteKind.Player, assets.tile`trap`, function(sprite: Hero, location: tiles.Location) {
+    let fuego = sprites.create(assets.image`fuego`, SpriteKind.Obstacle)
+    animation.runImageAnimation(fuego, assets.animation`fire noises`, 40, false)
+    tiles.placeOnTile(fuego, location)
+    mySprite.hit(1)
+    timer.after(100, function() {
+        sprites.destroy(fuego)
+    })
+})
+//BOSS TRIGGERS TRAP
+scene.onOverlapTile(SpriteKind.MrsEgan, assets.tile`trap`, function (sprite: MrsEgan, location: tiles.Location) {
+    let fuego = sprites.create(assets.image`fuego`, SpriteKind.Obstacle)
+    animation.runImageAnimation(fuego, assets.animation`fire noises`, 40, false)
+    tiles.placeOnTile(fuego, location)
+    sprite.hit(1)
+    timer.after(100, function () {
+        sprites.destroy(fuego)
+    })
+    console.log(`I GOT EM'!!!!`)
+})
+
+/*sprites.onOverlap(SpriteKind.MrsEgan, SpriteKind.Obstacle, function(enemy: MrsEgan, obstacle: Sprite) {
+    
+    enemy.hit(1)
+})
+*/
+//DASH
+controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
+    dash()
+})
+
+//ANIMATIONS
+//#8
+controller.up.onEvent(ControllerButtonEvent.Pressed, function() {
+    
+    animation.runImageAnimation(mySprite, assets.animation`Dane walk Backward`, 150, false)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function() {
+    animation.runImageAnimation(mySprite, assets.animation`Dane walk forward`, 150, false)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function() {
+    animation.runImageAnimation(mySprite, assets.animation`Dane walk left`, 150, false)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function() {
+    animation.runImageAnimation(mySprite, assets.animation`Dane walk right`, 150, false)
+})
+
+//MAIN
+let mySprite = new Hero(assets.image`myImage`, SpriteKind.Player)
+tiles.setCurrentTilemap(levels[levelNumber])
+mySprite.setStayInScreen(true)
+scene.cameraFollowSprite(mySprite)
+mySprite.setPosition(25,95)
+controller.moveSprite(mySprite, hero_velocity, hero_velocity)
+
+
+animation.runImageAnimation(snail,assets.animation`helpful snail idle`,350,true)
+snail.setPosition(100, 90)
+snail.sayText("You should totaly go in the suspicious purple hole!")
+
+scene.setBackgroundColor(6)
---
+++ tilemap.g.ts
@@ -0,0 +1,105 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level0":
+            case "level1":return tiles.createTilemap(hex`1000100002020202020402020202020202020202020102020202040201020302020204020202020203020202020202020202010202020402020302020202020204040402040304010202020102040202010202040202020202020203020203020402020404020202040406060602020104020202020403030202060506040202020302020202020202020606060402020202020402020402020102020301020203020204020204040402030402020302020203020202020102020204040402020202020202030304020404020204040201020102020202040404020202020402020203040202020102010402030201020202020402020202030202020202040202020404`, img`
+. . . . . . . . . . . . . . . . 
+. 2 . . . . . . 2 . . . . . . . 
+. . . . . . . . . . . . . . 2 . 
+. . . . . . . . . . . . . . . . 
+. . . 2 . . . 2 . . . . 2 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . 2 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 2 . . . 2 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 2 . . . . . . . . . . . . 
+. . . . . . . . . . . . 2 . 2 . 
+. . . . . . . . . . . . . . . . 
+. . . 2 . 2 . . . . 2 . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.builtin.forestTiles0,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass1,sprites.swamp.swampTile9,sprites.dungeon.floorLight0], TileScale.Sixteen);
+            case "level4":
+            case "level3":return tiles.createTilemap(hex`100010000f0c0c0c0c0c0c0c0c0c0c0c0c0c0c0e0f01040101010201010202020101120d0701040202020101010401020404010d0701020101040102030101010103010d0701040101010102010101010301020d0701030101010302010104010101010d0701010101040101020101010404010d0702030101010105050202010101010d0701040202010105050101030102040d0701030101010104030101010402020d0701010101020201030101010301010d100606060606080301090606060606110f0c0c0c0c0c0b01040a0c0c0c0c0c0e0703010304040301010103010101030d0701010101040102010401040401010d10060606060606060606060606060611`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 . . 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 . . 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.floorLight2,sprites.dungeon.floorDark2,sprites.dungeon.floorLight5,sprites.dungeon.floorDark5,sprites.dungeon.floorDarkDiamond,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleInnerSouthEast,sprites.swamp.swampTile9], TileScale.Sixteen);
+            case "level5":
+            case "level2":return tiles.createTilemap(hex`10001000090a0a0a0a0a0a0a0a0a0a0a0a0a0a0b080103010101040101040404010101070801030404040101011718181817010708040401010301040218110e101801070801030119010104011812161318040708010201010102040118140f15180107080101010103010104171818181701070804020101010105050404010101010708010304040101190501010201040307080102010101010302010101030404070801010101040401020101010201010708010201190101010401011902010107080101010301010201040101010102070802010203030201010102010101020708010101010301040103010303010107060d0d0d0d0d0d0d0d0d0d0d0d0d0d0c`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.floorLight2,sprites.dungeon.floorLight5,sprites.dungeon.floorDark5,sprites.dungeon.floorDark2,sprites.dungeon.floorDarkDiamond,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth0,sprites.builtin.forestTiles6,myTiles.tile1,sprites.builtin.forestTiles7,sprites.builtin.forestTiles5,sprites.builtin.forestTiles9,sprites.builtin.forestTiles11,myTiles.tile2,myTiles.tile3,sprites.builtin.forestTiles10,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.darkGroundCenter,myTiles.tile7], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "Broken wall horizontal":
+            case "tile4":return tile4;
+            case "myTile2":
+            case "tile5":return tile5;
+            case "myTile3":
+            case "tile6":return tile6;
+            case "trap":
+            case "tile7":return tile7;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
